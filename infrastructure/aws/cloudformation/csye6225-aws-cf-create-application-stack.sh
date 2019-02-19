9187a949b913703ac3390a015260778c9d07fba5
Zehua <ma.zeh@husky.neu.edu>
2019-02-18 20:47:58 -0500

    update yaml

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Input the stack name which you want to create"
 read name
-aws cloudformation update-stack --stack-name $name --template-body file://csye6225-cf-networking.yaml
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-application.yaml
 echo "Processing, please wait"
 aws cloudformation wait stack-create-complete --stack-name $name
 aws cloudformation describe-stacks

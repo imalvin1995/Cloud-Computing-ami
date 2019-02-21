1a8d674f056801299503b185a734ef337acfb101
Yu-Chiao Huang <huang.yuc@husky.neu.edu>
2019-02-21 00:46:19 -0500

    update

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-application-stack.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 set -e
+amiId=`aws ec2 describe-images --owners self --filters "Name=root-device-type,Values=ebs" | grep -o '"ImageId": *"[^"]*"' | grep -o '"[^"]*"$' | sed 's/\"//g' | head -n 1`
 echo "Input the stack name which you want to create"
 read name
-aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-application.yaml
+echo "Input the name of stack you want to refer"
+read refStackName
+aws cloudformation create-stack --stack-name $name --template-body file://csye6225-cf-application.yaml --parameters "ParameterKey=refStackName,ParameterValue=$refStackName" "ParameterKey=amiId,ParameterValue=$amiId"
 echo "Processing, please wait"
 aws cloudformation wait stack-create-complete --stack-name $name
 aws cloudformation describe-stacks
